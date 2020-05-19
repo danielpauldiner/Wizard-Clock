3b8b081690fd5e3643ef3203166ed8bd96508c1c
Joshua Dean Padiernos <jdmpadiernos@gmail.com>
2020-05-18 20:53:55 -0700

    Add podfile, firebase auth, and firestore configure

---
+++ Pods/Target Support Files/Pods-The Wizard Clock-The Wizard ClockUITests/Pods-The Wizard Clock-The Wizard ClockUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_The_Wizard_Clock_The_Wizard_ClockUITests : NSObject
+@end
+@implementation PodsDummy_Pods_The_Wizard_Clock_The_Wizard_ClockUITests
+@end
