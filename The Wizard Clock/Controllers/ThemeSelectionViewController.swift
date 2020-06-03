4ab9dca48bc273a3fe57ae740fc90e8336f99f9c
dinerd <dinerd@oregonstate.edu>
2020-06-03 18:06:14 -0400

    Added a header tot the theme table and had the default option start highlighted. Added comments to file.

--- The Wizard Clock/Controllers/ThemeSelectionViewController.swift
+++ The Wizard Clock/Controllers/ThemeSelectionViewController.swift
@@ -10,24 +10,43 @@ import UIKit
 
 class ThemeSelectionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    // Name of the theme to be displayed. Sent to the theme results view
     var themeName = "Nature"
+    // Boolean which determines if the theme should be drawn in greyscale. Sent to the theme results view
     var greyscale = "Off"
+    // List of all possible themes which populate the theme table
     let themes = ["Nature", "Jersey", "Binary"]
+    // Name of identifier for cells which is used in making the theme table
     let cellReuseIdentifier = "cell"
     
+    // Connects to the table for selecting themes in the storyboard
     @IBOutlet weak var ThemeSelectionTable: UITableView!
     
+    // Connects to the buttons for selecting the greyscale option in the storyboard
     @IBOutlet weak var greyscaleYesButton: UIButton!
     @IBOutlet weak var greyscaleNobutton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Set up a table for selecting the theme desired
         self.ThemeSelectionTable.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
         ThemeSelectionTable.delegate = self
         ThemeSelectionTable.dataSource = self
+        
+        // Display a header at the top of the table
+        let tableHeader = UILabel(frame: CGRect(x: 0, y: 0, width: ThemeSelectionTable.frame.width, height: 35))
+        tableHeader.font = UIFont.systemFont(ofSize: 25.0)
+        tableHeader.textAlignment = NSTextAlignment.center
+        tableHeader.text = "Theme Options"
+        ThemeSelectionTable.tableHeaderView = tableHeader
+        
+        // Highlights the first row of the table so that the user knows it is the default
+        let indexPath = IndexPath(row: 0, section: 0)
+        ThemeSelectionTable.selectRow(at: indexPath, animated: true, scrollPosition: .none)
     }
     
+    // Controls the button selection for the greyscal option
     @IBAction func greyscaleChanged(_ sender: UIButton) {
         greyscaleYesButton.isSelected = false
         greyscaleNobutton.isSelected = false
@@ -36,10 +55,12 @@ class ThemeSelectionViewController: UIViewController, UITableViewDelegate, UITab
         print(greyscale)
     }
     
+    // Performs seque for going to the theme results page
     @IBAction func displayThemePressed(_ sender: UIButton) {
         self.performSegue(withIdentifier: "goToResults", sender: self)
     }
     
+    // Prepares seque for going to the theme results page
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToResults" {
             let destinationVC = segue.destination as! ThemeResultsViewController
@@ -48,10 +69,12 @@ class ThemeSelectionViewController: UIViewController, UITableViewDelegate, UITab
         }
     }
     
+    // Sets the number of cells in the theme table
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.themes.count
     }
     
+    // Creates the cells for the theme table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = (self.ThemeSelectionTable.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
 
@@ -60,17 +83,9 @@ class ThemeSelectionViewController: UIViewController, UITableViewDelegate, UITab
         return cell
     }
     
+    // Sets the themeName parameter based on the table selection
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         themeName = self.themes[indexPath.row]
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
